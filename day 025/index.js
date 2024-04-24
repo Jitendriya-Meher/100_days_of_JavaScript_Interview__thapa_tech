b627a8aeadab63286e1aaa4883055daa3c1359c7
Jitendriya-Meher <jiten@gcekbpatna.ac.in>
2024-04-24 10:56:46 +0530

    day 25

---
+++ day 025/index.js
@@ -0,0 +1,13 @@
+
+function factorial( n){
+    if( n == 0){
+        return 1;
+    }
+
+    return n * factorial( n-1);
+}
+
+console.log(factorial(3));
+console.log(factorial(4));
+console.log(factorial(5));
+console.log(factorial(10));
